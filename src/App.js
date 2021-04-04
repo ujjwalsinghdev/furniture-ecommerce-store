76eef40aa45a861a6b582254c444e1f1a60f4b90
Ujjwal Singh <ujjwalsingh@MBP.local>
2021-04-05 00:03:22 +0530

    navbar & sidebar

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { Navbar, Sidebar, Footer } from "./components"
+
+import { Home, SingleProduct, Cart, Checkout, Error, About, Products, PrivateRoute } from "./pages"
+
+function App() {
+  return (
+    <Router>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route exact path="/about">
+          <About></About>
+        </Route>
+        <Route exact path="/cart">
+          <Cart></Cart>
+        </Route>
+        <Route exact path="/products">
+          <Products></Products>
+        </Route>
+        <Route exact path="/products/:id" children={<SingleProduct></SingleProduct>}></Route>
+
+        <Route exact path="/checkout">
+          <Checkout></Checkout>
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
+      <Footer></Footer>
+    </Router>
+  )
+}
+
+export default App
